4129ef10cc47d8b4e731fe91046c2cfa63fe0b79
Zhimin Tang <1003306213@qq.com>
2020-10-29 11:26:35 +0800

    updata DDP args

--- train.py
+++ train.py
@@ -204,7 +204,7 @@ def main(args):
 
     model_without_ddp = model
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         model_without_ddp = model.module
 
     if args.test_only:
